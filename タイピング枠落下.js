3d7ecbbed8f76ab9632ef45662d692e2e2b14841
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-10-16 12:39:11 +0900

    textの横長さ調整

--- タイピング枠落下.js
+++ タイピング枠落下.js
@@ -2,7 +2,7 @@ const canvas=document.getElementById("canvas");
 const ctx=canvas.getContext("2d");
 import textWarehouse from "./タイピング枠落下.json" assert{type:"json"};
 var text=[],
-    enAnswer="kokugo",count=0,i;
+    enAnswer=count=0,i;
 function drawBlock(letter){
     ctx.beginPath();
     ctx.fillStyle="#000000";
@@ -22,10 +22,14 @@ function drawBlock(letter){
 function main(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     count++;
-    if (count%100==0){
-        var number=Math.floor(Math.random()*textWarehouse.length);
-        text.push({x:Math.floor(Math.random()*(750-textWarehouse[number].en.length*12))+9,y:0,
-                   jn:textWarehouse[number].jn,en:textWarehouse[number].en});
+    if (count%500==0){
+        var number=Math.floor(Math.random()*textWarehouse.length),textX;
+        if (textWarehouse[number].jn>textWarehouse[number].en*2){
+            textX=Math.floor(Math.random()*(750-textWarehouse[number].jn.length*16))+9;
+        }else{
+            textX=Math.floor(Math.random()*(750-textWarehouse[number].en.length*12))+9;
+        }
+        text.push({x:textX,y:0,jn:textWarehouse[number].jn,en:textWarehouse[number].en});
     }
     for (i=0;i<text.length;i++){
         text[i].y+=0.25;
