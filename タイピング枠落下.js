f96dec22457b2961bc8967c739c6970625d3ecfe
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-10-17 23:42:29 +0900

    幅調整

--- タイピング枠落下.js
+++ タイピング枠落下.js
@@ -9,11 +9,10 @@ function drawBlock(letter){
     ctx.fillStyle="#000000";
     ctx.font="24px Arial";
     if (letter.en.length>letter.jn.length*2){
-        ctx.fillText(letter.en,letter.x+6,letter.y);
+        ctx.fillText(letter.en,letter.x,letter.y);
+        ctx.fillText(letter.jn,letter.x+(letter.en.length-letter.jn.length*2)*6,letter.y-24);
         var inum=(letter.en.match(new RegExp('['+"iftjl"+']','g'))||[]).length||0;
-        ctx.fillText(letter.jn,letter.x+(letter.en.length-letter.jn.length*2)*6+6,letter.y-24);
-        ctx.rect(letter.x-12,letter.y-51,letter.en.length*15,57);
-        console.log(inum);
+        ctx.rect(letter.x-8,letter.y-51,letter.en.length*12+16-(inum-1)*4,57);
     }else{
         ctx.fillText(letter.en,letter.x+(letter.jn.length*2-letter.en.length)*6,letter.y);
         ctx.fillText(letter.jn,letter.x,letter.y-24);
@@ -34,10 +33,10 @@ function main(){
     if (count%75==0){
         var number=Math.floor(Math.random()*textWarehouse.length),textX;
         if (textWarehouse[number].en.length>textWarehouse[number].jn.length*2){
-            textX=Math.floor(Math.random()*(750-textWarehouse[number].jn.length*24))+9;
+            textX=Math.floor(Math.random()*(750-textWarehouse[number].jn.length*24))-8;
             console.log(textWarehouse[number].jn);
         }else{
-            textX=Math.floor(Math.random()*(750-textWarehouse[number].en.length*12-24))+9;
+            textX=Math.floor(Math.random()*(750-textWarehouse[number].en.length*12))-8;
         }
         text.push({x:textX,y:0,jn:textWarehouse[number].jn,en:textWarehouse[number].en});
     }
