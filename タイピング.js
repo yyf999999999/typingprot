c9990b53195c6eb10396fd67fd89707099d92ea0
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-10-23 22:34:58 +0900

    enInput text忘れ

--- タイピング.js
+++ タイピング.js
@@ -164,7 +164,7 @@ function input(e){
                 if (!tfC){
                     enInput.text=enInput.text.slice(0,-1);
                     console.log("前:"+printCharacter+","+ans,enInput,enInput.length);
-                    if (enInput.length>0) printCharacter=back.printCharacter;ans=back.ans;
+                    if (enInput.text.length>0) printCharacter=back.printCharacter;ans=back.ans;
                     console.log("後:"+printCharacter+","+ans,enInput,enInput.length);
                 }
             }
