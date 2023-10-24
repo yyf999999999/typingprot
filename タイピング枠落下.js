2246265dccd1ba1e6014d774afdae12096cabd86
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-10-24 10:46:19 +0900

    ゲームクリアを時間処理に変更

--- タイピング枠落下.js
+++ タイピング枠落下.js
@@ -2,8 +2,8 @@ import textWarehouse from "./タイピング枠落下.json" assert{type:"json"};
 import {enToJn,enInput,enAnswer,enWrite,back} from "./タイピング.js";
 const canvas=document.getElementById("canvas");
 const ctx=canvas.getContext("2d");
-var text=[],fText=[],count=0,letter,i,interval,countInterval,dropSpeed,numberSubject;
-countInterval=250;dropSpeed=0.5;numberSubject=7000/countInterval;
+var text=[],fText=[],count=0,letter,i,interval,countInterval,dropSpeed;
+countInterval=250;dropSpeed=0.5;
     function drawBlock(letter){
         if (enWrite.text!=""&&i==0){
             letter.en=enWrite.text;
@@ -45,8 +45,7 @@ countInterval=250;dropSpeed=0.5;numberSubject=7000/countInterval;
         ctx.fillStyle="#FF0000";
         ctx.fillText("0点ライン",2,510);
         
-        if (count%countInterval==0&&numberSubject>0){
-            numberSubject--;
+        if (count%countInterval==0){
             var number=Math.floor(Math.random()*textWarehouse.length),textX;
             if (textWarehouse[number].en.length>textWarehouse[number].jn.length*2){
                 textX=Math.floor(Math.random()*(750-textWarehouse[number].jn.length*24-16))+9;
@@ -84,13 +83,13 @@ countInterval=250;dropSpeed=0.5;numberSubject=7000/countInterval;
         if (fText.length>0) if (fText[0].y<-39){
             fText.shift();
         }
-        if (5250>count&&count>=5000){
-            if (count==5000) countInterval/=2;
+        if (4750>count&&count>=4500){
+            if (count==4500) countInterval/=2;
             ctx.font="36px Arial";
             ctx.fillStyle="#000000";
             ctx.fillText("試験期間",(canvas.width-144)/2,(canvas.height-36)/2);
         }
-        if (numberSubject<=0&&text.length>0){
+        if (count>=6000){
             console.log("GAMECLEAR");
             ctx.font="36px Arial";
             ctx.fillStyle="#000000";
