046d27de4cefff7bd6b7cea1e7a82f7e688e6c3d
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-10-26 10:55:50 +0900

    元に戻す

--- タイピング枠落下.js
+++ タイピング枠落下.js
@@ -10,38 +10,36 @@ countInterval=300;
             letter.en=enWrite.text;
         }
         ctx.beginPath();
-        ctx.fillStyle = "#000000";
-        ctx.font = "24px Arial";
-        if (letter.en.length > letter.jn.length * 2) {
-            ctx.rect(letter.x - 16, letter.y - 51, letter.en.length * 12 + 32, 57);
-        } else {
-            ctx.rect(letter.x - 8, letter.y - 51, letter.jn.length * 24 + 16, 57);
+        ctx.fillStyle="#000000";
+        ctx.font="24px Arial";
+        if (letter.en.length>letter.jn.length*2){
+            ctx.rect(letter.x-16,letter.y-51,letter.en.length*12+32,57);
+        }else{
+            ctx.rect(letter.x-8,letter.y-51,letter.jn.length*24+16,57);
         }
-        ctx.lineWidth = 1;
-        if (i == 0) {
-            ctx.strokeStyle = "#FF0000";
-            ctx.fillStyle = "#FFFFFF";
-        } else {
-            ctx.strokeStyle = "#000000";
-            ctx.fillStyle = "#FFFFFF";
+        ctx.lineWidth=1;
+        if (i==0){
+            ctx.strokeStyle="#FF0000";
+            ctx.fillStyle="#FFFFFF";
+            //console.log(letter.enC,typeof(letter.enC));
+        }else{
+            ctx.strokeStyle="#000000";
+            ctx.fillStyle="#FFFFFF";
         }
-        ctx.fill();
-        ctx.stroke();
-        if (letter.en.length > letter.jn.length * 2) {
-    ctx.fillText(letter.en, letter.x, letter.y);
-    ctx.fillText(letter.jn, letter.x + (letter.en.length - letter.jn.length * 2) * 6, letter.y - 24);
-        } else {
-            ctx.fillText(letter.en, letter.x + (letter.jn.length * 2 - letter.en.length) * 6, letter.y);
-    ctx.fillText(letter.jn, letter.x, letter.y - 24);
+        ctx.fill();ctx.stroke();ctx.closePath();
+        if (letter.en.length>letter.jn.length*2){
+            ctx.fillText(letter.en,letter.x,letter.y);
+            ctx.fillText(letter.jn,letter.x+(letter.en.length-letter.jn.length*2)*6,letter.y-24);
+        }else{
+            ctx.fillText(letter.en,letter.x+(letter.jn.length*2-letter.en.length)*6,letter.y);
+            ctx.fillText(letter.jn,letter.x,letter.y-24);
         }
-        if (i == 0) {
-            ctx.fillStyle = "FF0000";
-            if (letter.en.length > letter.jn.length * 2) var tX = letter.x;
-            else var tX = letter.x + (letter.jn.length * 2 - letter.en.length) * 6;
-            ctx.fillText(enInput.text, tX, letter.y);
-            enAnswer.text = letter.enC;
+        if (i==0){
+            ctx.fillStyle="FF0000";
+            if (letter.en.length>letter.jn.length*2)var tX=letter.x; else var tX=letter.x+(letter.jn.length*2-letter.en.length)*6
+            ctx.fillText(enInput.text,tX,letter.y);
+            enAnswer.text=letter.enC;
         }
-        ctx.closePath();
     }
     function main(){
         count++;
