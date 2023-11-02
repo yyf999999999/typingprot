2e72adc1e3c3d3d5a8e5881a1aad8aa1fa62604b
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-11-02 13:41:26 +0900

    試験ゲージ修正

--- js/タイピング枠落下.js
+++ js/タイピング枠落下.js
@@ -110,9 +110,9 @@ function main(){
     drawText(lineCtx,"24px Arial","#000000","60点ライン",36,24);
     drawText(lineCtx,"24px Arial","#FF0000","0点ライン",48,510);
     drawText(timeCtx,"24px Arial","#000000","残り時間:"+Math.ceil(60-(count-controlCount)/100)+"秒",2,24);
-    drawText(timeCtx,"24px Arial","#000000","試験期間ゲージ",2,476)
+    drawText(timeCtx,"24px Arial","#000000","試験ゲージ",2,476)
     drawOBlock({context:timeCtx,x:2,y:478,width:146,height:32,fillColor:"#FFFFFF",strokeColor:"#000000"});
-    drawOBlock({context:timeCtx,x:2,y:478,width:146+(typeNumber.part/typeNumber.standard),height:32,fillColor:"#0000FF",strokeColor:"#0000FF"});
+    drawOBlock({context:timeCtx,x:2,y:478,width:146*(typeNumber.part/typeNumber.standard),height:32,fillColor:"#0000FF",strokeColor:"#000000"});
     if (count%countInterval==0){
         var number=0,textX;
         while (typeNumber.limit<textWarehouse[number].en.length){
